91c591fd4bc72c6192ef13b6bdfd8b0ada4030b7
Jiwon Park <qwes8873@gmail.com>
2023-06-27 22:11:20 +0900

    Add initial codes

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3010;
+
+app.use(express.static("public")); // 정적 파일 폴더 설정
+
+// app.get("/hi", (req, res) => {
+//   res.send("Hello Jiwon!");
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
