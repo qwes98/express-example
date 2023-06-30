44bbfcf19e83026a379c2fc0779c00ef21e2bb5e
Jiwon Park <qwes8873@gmail.com>
2023-06-30 20:47:14 +0900

    Modify app

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const port = 3010;
 
 app.use(express.static("public")); // 정적 파일 폴더 설정
 
-// app.get("/hi", (req, res) => {
-//   res.send("Hello Jiwon!");
-// });
+app.get("/hi", (req, res) => {
+  res.send("Hello Jiwon!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
